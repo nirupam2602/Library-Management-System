ce73397b0057ddf381e0b4c100fee7a119dabf67
Nirupam Kumar <nirupamkumar2003@gmail.com>
2025-04-25 23:56:35 +0530

    Update app.sql

--- app.sql
+++ app.sql
@@ -10,8 +10,8 @@ USE LibraryDB;
 -- Create table for books
 CREATE TABLE Books (
     BookID INT PRIMARY KEY AUTO_INCREMENT,
-    Title VARCHAR(200) NOT NULL,
-    Author VARCHAR(200),
+    Title VARCHAR(100) NOT NULL,
+    Author VARCHAR(100),
     Genre VARCHAR(100),
     PublishedYear INT
 );
@@ -19,8 +19,8 @@ CREATE TABLE Books (
 -- Create table for borrowers
 CREATE TABLE Borrowers (
     BorrowerID INT PRIMARY KEY AUTO_INCREMENT,
-    Name VARCHAR(200) NOT NULL,
-    Email VARCHAR(200) UNIQUE NOT NULL
+    Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL
 );
 
 -- Create table for tracking book loans
