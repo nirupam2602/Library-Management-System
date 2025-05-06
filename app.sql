118ce745bd56e32d7caadb177a22806f49894e54
Nirupam Kumar <nirupamkumar2003@gmail.com>
2025-05-06 23:56:33 +0530

    Update app.sql

--- app.sql
+++ app.sql
@@ -20,7 +20,7 @@ CREATE TABLE Books (
 CREATE TABLE Borrowers (
     BorrowerID INT PRIMARY KEY AUTO_INCREMENT,
     Name VARCHAR(91) NOT NULL,
-    Email VARCHAR(99) UNIQUE NOT NULL
+    Email VARCHAR(91) UNIQUE NOT NULL
 );
 
 -- Create table for tracking book loans
