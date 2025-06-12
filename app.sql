df421a7da208cb8af32e88c9570702e0e64e0a2d
Nirupam Kumar <nirupamkumar2003@gmail.com>
2025-06-12 23:59:55 +0530

    Update app.sql

--- app.sql
+++ app.sql
@@ -11,7 +11,7 @@ USE LibraryDB;
 CREATE TABLE Books (
     BookID INT PRIMARY KEY AUTO_INCREMENT,
     Title VARCHAR(89) NOT NULL,
-    Author VARCHAR(89),
+    Author VARCHAR(83),
     Genre VARCHAR(99),
     PublishedYear INT
 );
